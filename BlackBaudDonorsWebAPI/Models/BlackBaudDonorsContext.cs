18263ceabbced9614d14139a96975b3d78e23985
soakes84 <spencer.oakes84@gmail.com>
2018-04-12 22:25:41 -0400

    Setup DbContext

---
+++ BlackBaudDonorsWebAPI/Models/BlackBaudDonorsContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlackBaudDonorsWebAPI.Models
+{
+    public class BlackBaudDonorContext : DbContext
+    {
+        public BlackBaudDonorContext(DbContextOptions<BlackBaudDonorContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Donor> Donors { get; set; }
+    }
+}
--- BlackBaudDonorsWebAPI/Startup.cs
+++ BlackBaudDonorsWebAPI/Startup.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlackBaudDonorsWebAPI.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -20,13 +22,12 @@ namespace BlackBaudDonorsWebAPI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<BlackBaudDonorsContext>(opt => opt.UseInMemoryDatabase("BlackBaudDonors"));
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
